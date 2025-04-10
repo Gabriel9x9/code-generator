a6cde1d35834d24768b851a1faf6602fc7d28907
halo <lyg3044@qq.com>
2025-04-11 00:00:33 +0800

    first:basic+acm

---
+++ demo-projects/acm-template/src/com.halo/acm/MainTemplate.java
@@ -0,0 +1,29 @@
+package com.halo.acm;
+
+
+import java.util.Scanner;
+
+public class MainTemplate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNext()){
+            // get length
+            int n=scanner.nextInt();
+            // read array
+            int[] arr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=scanner.nextInt();
+            }
+
+            // logic
+            int sum=0;
+            for(int num:arr){
+                sum+=num;
+            }
+            System.out.println(("Sum: " + sum));
+        }
+
+        scanner.close();
+    }
+}
