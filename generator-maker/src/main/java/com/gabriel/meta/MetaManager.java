ca364b11ec0a0c59c269bc7983043307dc0052f5
halo <lyg3044@qq.com>
2025-04-19 11:47:11 +0800

    健壮性优化（输入校验【异常类、校验类】,圈复杂度）

--- generator-maker/src/main/java/com/gabriel/meta/MetaManager.java
+++ generator-maker/src/main/java/com/gabriel/meta/MetaManager.java
@@ -22,25 +22,28 @@ public class MetaManager {
         return meta;
     }
     private static Meta initMeta() {
+
         String metaJson= ResourceUtil.readUtf8Str("meta.json");
         Meta newMeta= JSONUtil.toBean(metaJson,Meta.class);
-        Meta.FileConfig fileConfig=newMeta.getFileConfig();
+
+//        Meta.FileConfig fileConfig=newMeta.getFileConfig();
         // todo 校验和处理默认值
+        MetaValidator.doValidAndFill(newMeta);
         return newMeta;
     }
 }
 
-class MetaManager2{
-    private static final Meta meta=initMeta();
-
-    private MetaManager2(){}
-
-    public static Meta getMetaObject(){
-        return meta;
-    }
-
-    private static Meta initMeta(){
-        // init
-        return meta;
-    }
-}
+//class MetaManager2{
+//    private static final Meta meta=initMeta();
+//
+//    private MetaManager2(){}
+//
+//    public static Meta getMetaObject(){
+//        return meta;
+//    }
+//
+//    private static Meta initMeta(){
+//        // init
+//        return meta;
+//    }
+//}
